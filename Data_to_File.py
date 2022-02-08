cfbc1e32dd6aa89220f216b3d605d3e66ba49b21
Simone Monaco <22541888+buonleandro@users.noreply.github.com>
2022-02-08 19:27:10 +0100

    Update Data_to_File.py

--- Data_to_File.py
+++ Data_to_File.py
@@ -2,6 +2,7 @@ import os
 from Data_Processor import MergeMRIAndSave, ProcessMaskAndSave
 
 root = "./testing/HGG_LGG/"
+folder = "testing"
 patients = os.listdir(root)
 relPaths = [os.path.join(root,x) for x in patients]
 
@@ -14,5 +15,5 @@ for i,x in enumerate(relPaths, start=1):
         t1c = os.path.join(x, file) if file.__contains__('_T1c') else t1c
         t1 = os.path.join(x, file) if file.__contains__('_T1') else t1
         #mask = os.path.join(x, file).replace('\\','/') if file.__contains__('.OT.') else mask
-    MergeMRIAndSave(flair, t2, t1c, t1, i)
+    MergeMRIAndSave(flair, t2, t1c, t1, folder, i)
     #ProcessMaskAndSave(mask, i)
