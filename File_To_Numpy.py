17df30341a6ce9e89036712ba7b33195f27a6b67
Simone Monaco <22541888+buonleandro@users.noreply.github.com>
2022-02-08 19:32:00 +0100

    Update File_To_Numpy.py

--- File_To_Numpy.py
+++ File_To_Numpy.py
@@ -2,9 +2,11 @@ import os
 from imageio import imread
 import numpy as np
 
-x_out = np.zeros(shape=(110, 512, 512), dtype=np.float32)
-
+IMG_N = 110
 DATA_FLD = './testing/'
+NPY_NAME = "test"
+
+x_out = np.zeros(shape=({}, 512, 512).format(IMG_N), dtype=np.float32)
 
 i = 0
 
@@ -15,4 +17,4 @@ for f in files:
     if (name.__contains__('mri')):
         x_out[i] = imread(os.path.join(DATA_FLD,f))
         i+=1
-np.save(os.path.join(DATA_FLD, 'test.npy'), x_out)
+np.save(os.path.join(DATA_FLD, '{}.npy').format(NAME), x_out)
